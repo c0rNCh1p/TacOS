fe8d2911309371764e35f83b4533f2cf146fcb96
c0rNCh1p <chip@c0rnch1p.cc>
2024-05-24 07:18:01 +1000

    Update: 05/24/24 07:18

--- scripts/new_version.sh
+++ scripts/new_version.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
 #shellcheck disable=SC2086
+#!/bin/bash
 
-OLDVSN='v02.02.03'
+OLDVSN='v03.01.01'
 
 echo -e "\nThe current iso version is $OLDVSN"
 echo -e '\nEnter the new iso version'
@@ -9,11 +9,12 @@ read -rp '▸ ' NEWVSN
 
 echo -e '\nChanging version refs in archiso files'
 sed -i 's/'$OLDVSN'/'$NEWVSN'/gI' '../build_iso.sh'
+sed -i 's/'$OLDVSN'/'$NEWVSN'/gI' '../readme.md'
 sed -i 's/'$OLDVSN'/'$NEWVSN'/gI' '../tacOS/airootfs/etc/calamares/branding/tacOS/branding.desc'
 sed -i 's/'$OLDVSN'/'$NEWVSN'/gI' '../tacOS/airootfs/etc/dev-rel'
 sed -i 's/'$OLDVSN'/'$NEWVSN'/gI' '../tacOS/airootfs/etc/hostname'
 sed -i 's/'$OLDVSN'/'$NEWVSN'/gI' '../tacOS/profiledef.sh'
 
 echo -e "Iso version updated to $NEWVSN"
-sed -i 's/'$OLDVSN'/'$NEWVSN'/gI' "$0"
+sed -i 's/'$OLDVSN'/'$NEWVSN'/gI' "$0" # Refers to this file
 unset NEWVSN OLDVSN
