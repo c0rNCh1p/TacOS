2352ee7a44d2c48f8e7aa74a5c1f476d3c22da66
c0rnch1p <chip@c0rnch1p.cc>
2024-06-01 16:13:18 +1000

    Update: 06/01/24 16:13

--- tacOS/profiledef.sh
+++ tacOS/profiledef.sh
@@ -36,7 +36,7 @@ bootstrap_tarball_compression=(zstd -c -T0 --auto-threads=logical --long -19)
 file_permissions=(
 	['/etc/grub.d/40_custom']='0:0:755'
 	['/etc/gshadow']='0:0:600'
-	['/etc/polkit-1/rules.d']='0:0:750'
+	#['/etc/polkit-1/rules.d']='0:0:750'
 	['/etc/shadow']='0:0:600'
 	['/etc/sudoers.d']='0:0:700'
 	['/etc/wireguard']='0:0:700'
