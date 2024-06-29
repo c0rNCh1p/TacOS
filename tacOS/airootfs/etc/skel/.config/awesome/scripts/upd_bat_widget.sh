27e7d7f6c572fe357ef9ea81a5ed81bca19384c2
c0rnch1p <chip@c0rnch1p.cc>
2024-06-30 03:47:22 +1000

    Update: 06/30/24 03:47

--- tacOS/airootfs/etc/skel/.config/awesome/scripts/upd_bat_widget.sh
+++ tacOS/airootfs/etc/skel/.config/awesome/scripts/upd_bat_widget.sh
@@ -2,13 +2,13 @@
 
 FILES='/usr/share/lua/5.3/lain/widget/bat.lua'
 SEARCH_PATTERN='local timeout     = args.timeout or 30'
-REPLACE_PATTERN='local timeout     = args.timeout or 3'
+REPLACE_PATTERN='local timeout     = args.timeout or 5'
 
 # Iterate over each matching file
 for FILE in $FILES; do
-    if [ -f "$FILE" ]; then	grep -q "$SEARCH_PATTERN" "$FILE" &&
+	if [ -f "$FILE" ]; then grep -q "$SEARCH_PATTERN" "$FILE" &&
 		sudo sed -i "s/$SEARCH_PATTERN/$REPLACE_PATTERN/" "$FILE" &&
 		notify-send 'AwesomeWM' 'Timeout setting updated in bat.lua'
-    else notify-send 'AwesomeWM' 'ERR: bat.lua file not found at $FILE'
-    fi
+	else notify-send 'AwesomeWM' 'ERR: bat.lua file not found at $FILE'
+	fi
 done
