d2a0ca2a48974d1c1b467891f28fe3008a2f3b98
c0rnch1p <chip@c0rnch1p.cc>
2024-06-30 02:48:09 +1000

    Update: 06/30/24 02:48

---
+++ tacOS/airootfs/etc/skel/.config/wezterm/wezterm.lua
@@ -0,0 +1,37 @@
+--[[ 80x
+
+                \ \      /  __| __  / __ __|  __|  _ \   \  |
+                 \ \ \  /   _|     /     |    _|     /  |\/ |
+                  \_/\_/   ___| ____|   _|   ___| _|_\ _|  _|
+
+================================================================================
+
+ Files:
+ - Contributors: $HOME/.config/contrib
+ - Filepath: $HOME/.config/wezterm/wezterm.lua
+ - Licence: $HOME/.config/licence
+
+================================================================================
+
+ Resources:
+ - Git Repo: https://github.com/wez/wezterm
+ - Official Site: https://wezfurlong.org/wezterm/index.html
+
+--============================================================================]]
+-- [1] Environment
+--==============================================================================
+local wezterm=require('wezterm')
+local config={}
+if wezterm.config_builder then config=wezterm.config_builder() end
+--==============================================================================
+-- [2] Settings
+--==============================================================================
+config.color_scheme='theme2 (terminal.sexy)'
+config.font=wezterm.font_with_fallback({{family='Nimbus Mono PS', scale=0.8}})
+config.window_background_opacity=0.8
+config.window_decorations='RESIZE'
+config.enable_tab_bar=false
+config.scrollback_lines=3500
+config.default_workspace='home'
+config.status_update_interval=1000
+return config
